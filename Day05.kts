38814cfb90eb1f9c74c3e830ef8cf87c2f5d7e69
Gerald Burke <gerald.burke@omf.com>
2022-12-06 12:31:46 -0600

    Day 5

--- Day05.kts
+++ Day05.kts
@@ -6,20 +6,7 @@ import java.io.File
 
 val input = File("Data/Day05.txt").readText()
 
-val testInput = """
-    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2
-"""
-
-
-val positionInput = testInput
+val positionInput = input
 	.split("\n\n")
 	.first()
 	.split("\n")
@@ -29,22 +16,112 @@ val indices = positionInput
 	.split(" ")
 	.filter { !it.isBlank() }
 	.map { it.toInt() }
-println("Indices: " + indices) 
 
 val rows = positionInput
 	.dropLast(1)
 	.filter {
 		it.count() > 0
 	}
+
+var charIndices = arrayOf(1)
+for (index in indices.dropLast(1)) {
+	val next = charIndices.last() + 4
+	charIndices += next
+}
+
+val stacks = indices.map {
+	val index = it
+	rows.reversed().map {
+		if (it.isBlank()) {
+			null
+		} else {
+			it[charIndices[index - 1]]
+		}
+	}
+	.filter { it.toString() != " " }
+	.filterNotNull()
+	.toMutableList()
+}
+
+class Move(val from: Int, val to: Int, val length: Int) {}
+
+val moves = input.split("\n\n")
+	.last()
+	.split("\n")
+	.filter { !it.isBlank() }
 	.map {
-		it.filter { !it.isBlank() }
+		it.split(" ")
+			.map {
+				try {
+					it.toInt()
+				} catch(e: Exception) {
+					null
+				}
+			}
+			.filterNotNull()
+	}
+	.map {
+		Move(it[1], it[2], it[0])
 	}
 
-for (index in indices) {
-	println(index)
-	for (row in rows.reversed()) {
+// Part One
 
-		println(row)
-		println("First: " + row.first())
+var result1 = indices.map {
+	val index = it
+	rows.reversed().map {
+		if (it.isBlank()) {
+			null
+		} else {
+			it[charIndices[index - 1]]
+		}
+	}
+	.filter { it.toString() != " " }
+	.filterNotNull()
+	.toMutableList()
+}
+
+for (move in moves) {
+	val fromIndex = move.from - 1
+	val toIndex = move.to - 1 
+	for (i in 0..move.length - 1) {
+		if (!result1[fromIndex].isEmpty()) {
+			val box = result1[fromIndex].removeLast()
+			result1[toIndex] += box	
+		}
 	}
 }
+
+val output1 = result1.map { it.last() }
+println(output1.joinToString(""))
+
+// Part Two
+
+var result2 = indices.map {
+	val index = it
+	rows.reversed().map {
+		if (it.isBlank()) {
+			null
+		} else {
+			it[charIndices[index - 1]]
+		}
+	}
+	.filter { it.toString() != " " }
+	.filterNotNull()
+	.toMutableList()
+}
+
+for (move in moves) {
+	val fromIndex = move.from - 1
+	val toIndex = move.to - 1
+	if (!result2[fromIndex].isEmpty()) {
+		val boxes = (0..move.length - 1).map { 
+			result2[fromIndex].removeLast()
+		}
+		.reversed()
+		result2[toIndex] += boxes
+	}
+}
+
+val output2 = result2.map { it.last() }
+println(output2.joinToString(""))
+
